ed8fc33c7c13dd78652a558a89958888d1958e37
funkiiboyz-byte <funkiiboyz@gmail.com>
2026-04-03 11:48:41 +0600

    Recover workspace to previous branch snapshot

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+window.MPQM_FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyAZbiH17ijO0Osg2AGIq7xedv829u6Key4',
+  authDomain: 'question-maker-895f3.firebaseapp.com',
+  projectId: 'question-maker-895f3',
+  storageBucket: 'question-maker-895f3.firebasestorage.app',
+  messagingSenderId: '638094853519',
+  appId: '1:638094853519:web:3f1ac5cadcf0cc8d2a77fa',
+  measurementId: 'G-L2WXLTSHWH',
+};
+
+window.MPQM_CLOUDFLARE_API = 'https://megaprep-question-maker-project.funkiiboyz.workers.dev';
+window.MPQM_CLOUDFLARE_TOKEN = '9f8a7b6c5d4e3f2a1b0jhjkhkjyfy77665';
+window.MPQM_BACKEND = 'cloudflare';
